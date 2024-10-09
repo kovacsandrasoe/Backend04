505931ddd60238461fcdb1d8603c293d0057cbb8
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-09 16:02:41 +0200

    get done

---
+++ Backend04/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Backend04.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        static List<Movie> Movies = new List<Movie>()
+        {
+            new Movie() { Id = "1", Title = "The Shawshank Redemption", Rating = 9.3, IsReleased = true },
+            new Movie() { Id = "2", Title = "The Godfather", Rating = 9.2, IsReleased = true },
+            new Movie() { Id = "3", Title = "The Dark Knight", Rating = 9.0, IsReleased = true },
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return Movies;
+        }
+
+
+
+    }
+}
