414c1efc36247f0a821aa701f47c9ec99b027d2a
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-09 15:56:28 +0200

    models done

---
+++ Backend04/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend04.Models
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public double Rating { get; set; }
+
+        public bool IsReleased { get; set; }
+    }
+}
