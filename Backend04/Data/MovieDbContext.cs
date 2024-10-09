54c530f09006a65cb1eaa5032909a8b222b8d201
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2024-10-09 16:29:42 +0200

    dbcontex done

--- Backend04/Data/MovieDbContext.cs
+++ Backend04/Data/MovieDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace Backend04.Data
+﻿using Backend04.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend04.Data
 {
-    public class MovieDbContext
+    public class MovieDbContext : DbContext
     {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=MovieDb;Trusted_Connection=True;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
